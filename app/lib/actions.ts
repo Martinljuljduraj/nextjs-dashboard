f427dcf79af2589e4638474787066efd45c87c7a
Martin Lulgjuraj <martinlulgjuraj@mac.mynetworksettings.com>
2025-05-03 00:22:28 -0400

    fixed edit button issue

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -34,3 +34,34 @@ export async function createInvoice(formData: FormData) {
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
   }
+
+  // Use Zod to update the expected types
+  const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+  export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status } = UpdateInvoice.parse({
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+    });
+   
+    const amountInCents = amount * 100;
+
+   try {
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+   } catch (error) {
+    console.log(error)
+   }
+   
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+  }
+
+  export async function deleteInvoice(id: string) {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
+  }
